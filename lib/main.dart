1914079ac18192667fd2368d266bd7d948fac6b3
Diego Rocha <diegorsf.dns@gmail.com>
2023-05-20 20:59:31 -0300

    Challenge concluded

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,9 +17,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  List<int> randomNumber = [1, 2];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+      children: [
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                randomNumber = generateNumber();
+              });
+            },
+            child: Image.asset('images/dice${randomNumber[0]}.png'),
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                randomNumber = generateNumber();
+              });
+            },
+            child: Image.asset('images/dice${randomNumber[1]}.png'),
+          ),
+        )
+      ],
+    ));
   }
 }
+
+List<int> generateNumber() =>
+    List<int>.generate(2, (index) => Random().nextInt(6) + 1);
